b74bff2328e6c801bb82fe4b9c4abf52690402ec
ChiWhiteSox56 <cindymichalowski@yahoo.com>
2020-08-02 12:47:38 -0400

    Removed CoreData from project

--- LemonIceKingIOS/SceneDelegate.swift
+++ LemonIceKingIOS/SceneDelegate.swift
@@ -10,37 +10,21 @@ import UIKit
 import SwiftUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  var window: UIWindow?
 
-    var window: UIWindow?
+  func scene(
+    _ scene: UIScene,
+    willConnectTo _: UISceneSession,
+    options _: UIScene.ConnectionOptions
+  ) {
+    guard let windowScene = scene as? UIWindowScene
+    else { return }
 
-    func scene(_ scene: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-
-        // Get the managed object context from the shared persistent container.
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-
-        // Create the SwiftUI view and set the context as the value for the managedObjectContext environment keyPath.
-        // Add `@Environment(\.managedObjectContext)` in the views that will need the context.
-
-        // Use a UIHostingController as window root view controller.
-        if let windowScene = scene as? UIWindowScene {
-            let window = UIWindow(windowScene: windowScene)
-            
-            // create the SwiftUI view rhat provides the window contents
-            let contentView = ContentView(flavorStore: FlavorStore()).environment(\.managedObjectContext, context)
-
-            window.rootViewController = UIHostingController(rootView: contentView)
-            self.window = window
-            window.makeKeyAndVisible()
-        }
-    }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
+    let window = UIWindow(windowScene: windowScene)
+    window.rootViewController = UIHostingController(
+      rootView: ContentView( flavorStore: FlavorStore() )
+    )
+    self.window = window
+    window.makeKeyAndVisible()
+  }
 }
